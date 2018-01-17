0476820f3fea2c1194532a0ef173a20cee94040c
Lukas Kall <lukas.kall@scilifelab.se>
2018-01-17 14:19:58 +0100

    do not run mc_fdr in test (too slow for travis)

--- .test.sh
+++ .test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 cd example
 diffacto -i iPRG.novo.pep.csv -samples iPRG.samples.lst -out iPRG.denovo.protein.txt \
-  -mc_out iPRG.denovo.protein.FDR -min_samples 4 -impute_threshold 0.9 \
-  -use_unique True -log2 False
+  -min_samples 4 -impute_threshold 0.9 -use_unique True -log2 False
 protfile_size=$(wc -l < iPRG.denovo.protein.txt)
-fdrfile_size=$(wc -l < iPRG.denovo.protein.FDR)
-if [ $protfile_size -ge '200' ] && [ $fdrfile_size -ge '200' ]; then
+#fdrfile_size=$(wc -l < iPRG.denovo.protein.FDR)
+#if [ $protfile_size -ge '200' ] && [ $fdrfile_size -ge '200' ]; then
+if [ $protfile_size -ge '200' ]; then
     # All OK
     exit 0
 else
